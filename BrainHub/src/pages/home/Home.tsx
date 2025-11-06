e018834741e15a2009506ee77225cbf86ca52897
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-11-05 22:54:57 -0300

    Home page e libs

---
+++ BrainHub/src/pages/home/Home.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import type { Artigo } from "../../types/Artigo";
+import { ArtigoList } from "../../components/ArtigoList";
+
+export function Home() {
+    const [artigos, setArtigos] = useState<Artigo[]>([]);
+
+    useEffect(() => {
+        axios
+            .get<Artigo[]>("https://localhost:7138/api/Artigo", {
+                headers: { accept: "text/plain" },
+            })
+            .then((res) => setArtigos(res.data))
+            .catch((err) => console.error("Erro ao buscar artigos:", err));
+    }, []);
+
+    return (
+        <div>
+            Home Page
+
+
+            <h1>Lista de Artigos</h1>
+            <ArtigoList artigos={artigos} />
+        </div>
+    )
+}
