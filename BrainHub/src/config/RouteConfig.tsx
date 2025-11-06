fcef8f1206253a32d59be5e0d3033290e1de8283
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-11-05 22:54:27 -0300

    add route config

---
+++ BrainHub/src/config/RouteConfig.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/home/Home";
+
+const AppRouteConfig = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      {/* <Route path="*" element={<AppRoutes/>}/> */}
+    </Routes>
+  );
+};
+
+export default AppRouteConfig;
