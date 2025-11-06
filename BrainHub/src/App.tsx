3245a0c02449dd141ac9c2a8248cf8427f3dc79b
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-11-05 22:54:16 -0300

    reactor app

--- BrainHub/src/App.tsx
+++ BrainHub/src/App.tsx
@@ -1,25 +1,15 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { ArtigoList } from "./components/ArtigoList";
-import type { Artigo } from "./types/Artigo";
 import "./App.css";
+import { Footer } from "./components/layout/footer/Footer";
+import { Header } from "./components/layout/header/Header";
+import AppRouteConfig from "./config/RouteConfig";
 
 function App() {
-  const [artigos, setArtigos] = useState<Artigo[]>([]);
-
-  useEffect(() => {
-    axios
-      .get<Artigo[]>("https://localhost:7138/api/Artigo", {
-        headers: { accept: "text/plain" },
-      })
-      .then((res) => setArtigos(res.data))
-      .catch((err) => console.error("Erro ao buscar artigos:", err));
-  }, []);
 
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Lista de Artigos</h1>
-      <ArtigoList artigos={artigos} />
+      <Header/>
+      <AppRouteConfig />
+      <Footer/>
     </div>
   );
 }
