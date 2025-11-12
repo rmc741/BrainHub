90472f74eaa9ba4b0688c4184336c200f8a6b373
Rafael Mendonca <rafael.medonca2@gmail.com>
2025-11-12 19:55:17 -0300

    header layout e css

--- BrainHub/src/components/layout/header/Header.tsx
+++ BrainHub/src/components/layout/header/Header.tsx
@@ -1,7 +1,23 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./index.css";
+
 export function Header() {
     return(
-        <div>
-            Header
-        </div>
+        <AppBar sx={{display:"flex"}}>
+            <Toolbar sx={{justifyContent:"space-between"}}>
+                <a className="logo" href="/">
+                    <img src="teste.svg" style={{height:"50px"}} alt="Logo"/>
+                    <Typography>
+                        BarinHub
+                    </Typography>
+                </a>
+
+                <div className="nav-bar">
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/about">About</Link>
+                </div>
+            </Toolbar>
+        </AppBar>
     )
 }
