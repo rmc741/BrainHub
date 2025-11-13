2747e44d5cf28a87fe5816eafd39b5c5055905ad
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-11-13 20:19:50 -0300

    add footer componente

--- BrainHub/src/components/layout/footer/Footer.tsx
+++ BrainHub/src/components/layout/footer/Footer.tsx
@@ -1,7 +1,23 @@
+import { Box, Typography } from "@mui/material";
+
 export function Footer() {
-    return(
-        <div>
-            Footer
-        </div>
-    )
+  return (
+    <Box 
+      sx={{
+        backgroundColor: "#1976d2",
+        color: "white",
+        padding: "20px 0",
+        marginTop: "auto",
+        textAlign: "center"
+      }}
+    >
+      <Typography variant="body1">
+        © {new Date().getFullYear()} BarinHub — Todos os direitos reservados.
+      </Typography>
+
+      <Typography variant="caption" display="block" sx={{ marginTop: "10px" }}>
+        “Compartilhando ideias e construindo conhecimento juntos.”
+      </Typography>
+    </Box>
+  );
 }
